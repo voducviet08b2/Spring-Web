1264ac69e4fe818ee1b466d7dac59e4716b92a6e
viet <voducviet08b2@gmail.com>
2020-08-25 08:18:23 +0700

    update

---
+++ bai4/BaiTapQuanLySanPham/src/main/java/model/Product.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+
+    public Product(int id, String name, int price, String catagory) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.catagory = catagory;
+    }
+
+    private int price;
+    private String catagory;
+}
